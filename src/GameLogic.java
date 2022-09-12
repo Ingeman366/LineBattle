7e014e5d2efccc9672018d030b986d50ea446f27
simonj1704 <simo915m@stud.kea.dk>
2022-09-12 16:01:04 +0200

    Updated GameLogic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,2 +1,10 @@
 public class GameLogic {
+
+    private String attack;
+    private String forward;
+    private String retreat;
+    private boolean surrender;
+    private boolean dropBomb;
+    private boolean detonateBomb;
+    private String showStats;
 }
