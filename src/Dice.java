e09267959e5c3d0e44b6718519a10b84407bb7df
Ingeman366 <oliv366x@stud.kea.dk>
2022-09-12 15:54:16 +0200

    Opdateret Dice

--- src/Dice.java
+++ src/Dice.java
@@ -3,14 +3,11 @@ import java.util.Random;
 public class Dice {
     Random rng = new Random();
     private int value;
-    public int getDice() {
-        return value;
+
+    public int rollDice(){
+        return rng.nextInt(6)+1;
     }
 
-    public void setDice(int value) {
-        this.value = value;
-    }
 
-    private int value;
 
 }
