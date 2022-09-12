b0720591e4b8374d3a1619077dc2e1ac5af3ddac
Ingeman366 <oliv366x@stud.kea.dk>
2022-09-12 15:43:52 +0200

    Oprettede klasser

---
+++ src/Dice.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Dice {
+    Random rng = new Random();
+    private int value;
+    public int getDice() {
+        return value;
+    }
+
+    public void setDice(int value) {
+        this.value = value;
+    }
+
+    private int value;
+
+}
